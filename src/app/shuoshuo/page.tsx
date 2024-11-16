a76df9a90bbddb3179a9a986848bf711c33a633a
ciraos <frank.ka@foxmail.com>
2024-11-16 12:46:18 +0800

    updated .

--- src/app/shuoshuo/page.tsx
+++ src/app/shuoshuo/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
 import type { Metadata } from "next";
-import Aside from "@/components/aside";
-import Twikoo from "@/components/thirdParty/twikoo";
 import moment from "moment";
 import {sslist} from "@/config/shuoshuo"
+import Twikoo from "@/components/thirdParty/twikoo";
 
 export const metadata:Metadata = {
     title: "说说 - 葱苓sama",
@@ -14,29 +11,21 @@ export const metadata:Metadata = {
 export default function Shuoshuo() {
   return (
     <>
-      <div className="w-full max-w-6xl py-[40px] px-[15px] mx-auto flex max-768:flex-col">
-        <div className="w-3/4 rounded-xl max-768:w-full">
-          <div className="main-container py-2 px-6">
-            {/* 标题 */}
-            <div className="w-max h-16 my-0 mx-auto flex text-xl items-center"><p className="before:content-['────'] before:mr-2 after:content-['────'] after:ml-2">碎碎念</p></div>
-            {/* 容器 */}
-            <ul className="mt-2 px-2 mx-auto transition-all grid grid-cols-3 gap-2 max-425:block">
-              {sslist.map((item, index) => (
-                <li key={index} className="w-60 h-max bg-white rounded-xl leading-relaxed flex flex-col justify-between px-2 py-2 transition-all shadow-sm hover:shadow-md max-425:w-full dark:bg-dacard dark:text-[whitesmoke]">
-                  <p className="">{item.content}</p>
-                  <div className="my-1 columns-4 gap-1 transition-all hover:columns-3">{item.images}</div>
-                  <hr className="my-2" />
-                  <div className="w-full flex items-center justify-between text-sm">
-                    <span className="bg-slate-200 py-[1px] px-[4px] rounded-md before:content-['#'] before:mr-[1px]">{item.tag}</span>
-                    <span className="">{moment(item.time).format('YYYY-MM-DD')}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <Twikoo />
-        </div>
-        <Aside />
+      <div className="container max-w-6xl py-[40px] px-[15px] mx-auto">
+        <wc-waterfall id="" className="wc-ss" gap={0} cols={4}>
+          {sslist.map((item) => (
+              <div key={item.id} className="w-60 h-max bg-white rounded-xl flex flex-col justify-between py-2 px-2 shadow-sm hover:shadow-md max-425:w-full dark:bg-dacard dark:text-[whitesmoke]">
+                <p className="">{item.content}</p>
+                <div className="my-1 columns-4 gap-1 hover:columns-3">{item.images}</div>
+                <hr className="my-[1px]" />
+                <div className="w-full h-8 flex items-center justify-between text-sm">
+                  <span className="bg-slate-200 py-0 px-1 rounded-md before:content-['#'] before:mr-[1px]">{item.tag}</span>
+                  <span className="text-sm">{moment(item.time).format('YYYY-MM-DD')}</span>
+                </div>
+              </div>
+            ))}
+        </wc-waterfall>
+        <Twikoo />
       </div>
     </>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,8 +7,7 @@ import { navList } from "@/config/navlist";
 import avatar1 from "@/app/images/avatar1.avif";
 
 export default function Navbar() {
-  const [isOpen, setisOpen] = useState(false);
-  function addOpenClass() {}
+  const [comment, setComment] = useState(false);
   return (
     <>
       <div id="navbar" className="w-full max-w-6xl h-16 my-0 mx-auto py-0 px-2 flex rounded-b-xl shadow-lg hover:shadow-xl dark:bg-dacard">
@@ -23,34 +22,6 @@ export default function Navbar() {
                 <ul className="droplist hidden absolute w-24 mx-auto rounded-lg overflow-hidden divide-y-2 divide-solid divide-gray-400 bg-white">
                   {item.child.map((link, index) => (
                     <li key={index} className="w-full h-10 flex items-center text-base justify-center cotnent-center text-black hover:text-purple-500 text-[15.8px]"><Link href={link.link} className="flex items-center justify-center gap-1">{link.icon}{link.name}</Link></li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-          ))}
-        </ul>
-
-        <div id="search" className="search w-60 h-8 bg-slate-200 my-auto content-center gap-2 rounded-lg dark:text-white">
-          <form action="" className="px-4 flex items-center gap-0">
-            <Icon width="1.1em" height="1.1em" icon="iconamoon:search-duotone" />
-            <input type="text" placeholder="搜索..." className="bg-slate-200 ml-2 text-md" />
-          </form>
-        </div>
-
-        <ul id="dd_menu" className="dd_menu">
-          <li><Image src={avatar1} alt="avatar1" title="avatar1" className="w-20 h-20 mx-auto rounded-[50%]" /><hr className="my-2" /></li>
-          {navList?.map((item, index) => (
-            <li key={index} className="dd_menu_li">
-              <div className="dd_menu_li_div">
-                <div className="dd_menu_li_div_title">{item.list}</div>
-                <ul className="dd_menu_li_div_ul">
-                  {item.child.map((link, index) => (
-                    <li key={index} className="dd_menu_li_div_ul_li">
-                      <Link href={link.link} className="dd_menu_link">
-                        <div className="dd_menu_link_icon">{link.icon}</div>
-                        <div className="dd_menu_link_name">{link.name}</div>
-                      </Link>
-                    </li>
                   ))}
                 </ul>
               </div>
